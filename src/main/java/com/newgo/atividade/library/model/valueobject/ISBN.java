6596b697d3e65d986ba96fa440779ff08c08f121
NathanSoa <nathansoares809@gmail.com>
2022-11-28 16:18:53 -0300

    add format validation

--- src/main/java/com/newgo/atividade/library/model/valueobject/ISBN.java
+++ src/main/java/com/newgo/atividade/library/model/valueobject/ISBN.java
@@ -1,6 +1,8 @@
 package com.newgo.atividade.library.model.valueobject;
 
 import javax.persistence.Embeddable;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Embeddable
 public class ISBN {
@@ -11,11 +13,19 @@ public class ISBN {
 
     }
 
+    private boolean validate(String value) {
+        Pattern pattern = Pattern.compile("([0-9]{3}-?[0-9]{1}-?[0-9]{5}-?[0-9]{3}-?[0-9]{1})|([0-9]{1}-?[0-9]{5}-?[0-9]{3}-?[0-9]{1})");
+        Matcher matcher = pattern.matcher(value);
+        return matcher.matches();
+    }
+
     public String getNumber() {
         return number;
     }
 
     public void setNumber(String number) {
+        if(!validate(number))
+            throw new IllegalArgumentException("ISBN should be in valid format!");
         this.number = number;
     }
 }
