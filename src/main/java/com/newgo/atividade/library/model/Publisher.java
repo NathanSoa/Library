208e40b49d143c7892c8ae4fa1b9a9b54e8d9301
NathanSoa <nathansoares809@gmail.com>
2022-11-28 18:50:44 -0300

    refactor: publisher updated to an embedded value

--- src/main/java/com/newgo/atividade/library/model/Publisher.java
+++ src/main/java/com/newgo/atividade/library/model/Publisher.java
@@ -1,36 +1,25 @@
 package com.newgo.atividade.library.model;
 
-import javax.persistence.*;
-import java.util.Set;
+import javax.persistence.Embeddable;
 
-@Entity
+@Embeddable
 public class Publisher {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-
-    @OneToMany(mappedBy = "publisher")
-    private Set<Book> books;
 
     public Publisher() {
 
     }
 
-    public Long getId() {
-        return id;
+    public Publisher(String publisherName) {
+        this.publisherName = publisherName;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    private String publisherName;
+
+    public String getPublisherName() {
+        return publisherName;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+    public void setPublisherName(String name) {
+        this.publisherName = name;
     }
 }
