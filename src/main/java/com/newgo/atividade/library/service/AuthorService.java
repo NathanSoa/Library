3f405b3da07a50362fd4f3aec2e2e0f6fcbb3246
NathanSoa <nathansoares809@gmail.com>
2022-12-01 11:24:55 -0300

    refactor: deleted service interfaces and adapted them to return DTOs

--- src/main/java/com/newgo/atividade/library/service/AuthorService.java
+++ src/main/java/com/newgo/atividade/library/service/AuthorService.java
@@ -1,11 +1,71 @@
 package com.newgo.atividade.library.service;
 
 import com.newgo.atividade.library.model.Author;
+import com.newgo.atividade.library.dto.AuthorDTO;
+import com.newgo.atividade.library.model.valueobject.Name;
+import com.newgo.atividade.library.repository.AuthorRepository;
 
+import org.modelmapper.ModelMapper;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 import java.util.Set;
 
-public interface AuthorService extends CrudService<Author, Long> {
+@Service
+public class AuthorService {
 
-    boolean exists(Set<Author> authors);
-    boolean existById(Long id);
+    private final AuthorRepository authorRepository;
+    private final ModelMapper modelMapper;
+
+    public AuthorService(AuthorRepository authorRepository, ModelMapper modelMapper) {
+        this.authorRepository = authorRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    public void save(Author object) {
+        authorRepository.save(object);
+    }
+
+    public AuthorDTO findById(Long id) {
+        Optional<Author> author = authorRepository.findById(id);
+        return author.isPresent() ? modelMapper.map(author.get(), AuthorDTO.class) : null;
+    }
+
+    public Page<AuthorDTO> findAll(Pageable pageable) {
+        return authorRepository
+                .findAll(pageable)
+                .map(each -> modelMapper.map(each, AuthorDTO.class));
+    }
+
+    public AuthorDTO update(Long id, Author object) {
+        Optional<Author> authorOptional = authorRepository.findById(id);
+
+        if(!authorOptional.isPresent()) {
+            return null;
+        }
+
+        Author author = authorOptional.get();
+        BeanUtils.copyProperties(object, author, "id");
+        save(author);
+
+        return modelMapper.map(author, AuthorDTO.class);
+    }
+
+    public void deleteById(Long id) {
+        authorRepository.deleteById(id);
+    }
+
+    public boolean exists(Set<Author> authors) {
+        return authors.stream()
+                .map(Author::getId)
+                .allMatch(this::existsById);
+    }
+
+    public boolean existsById(Long id) {
+        return authorRepository.existsById(id);
+    }
 }
--- src/main/java/com/newgo/atividade/library/service/BookService.java
+++ src/main/java/com/newgo/atividade/library/service/BookService.java
@@ -1,7 +1,67 @@
 package com.newgo.atividade.library.service;
 
+import com.newgo.atividade.library.exception.InvalidAuthorException;
 import com.newgo.atividade.library.model.Book;
+import com.newgo.atividade.library.dto.BookDTO;
+import com.newgo.atividade.library.repository.BookRepository;
 
-public interface BookService extends CrudService<Book, Long> {
+import org.modelmapper.ModelMapper;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class BookService {
+
+    private final BookRepository bookRepository;
+    private final AuthorService authorService;
+    private final ModelMapper modelMapper;
+
+    public BookService(BookRepository bookRepository, AuthorService authorService, ModelMapper modelMapper) {
+        this.bookRepository = bookRepository;
+        this.authorService = authorService;
+        this.modelMapper = modelMapper;
+    }
+
+    public void save(Book object) {
+        if(authorService.exists(object.getAuthors()) && object.getAuthors().size() >= 1){
+            bookRepository.save(object);
+            return;
+        }
+
+        throw new InvalidAuthorException("An invalid author was sent!");
+    }
+
+    public BookDTO findById(Long id) {
+        Optional<Book> book = bookRepository.findById(id);
+        return book.isPresent() ? modelMapper.map(book.get(), BookDTO.class) : null;
+    }
+
+    public Page<BookDTO> findAll(Pageable pageable) {
+        return bookRepository
+                .findAll(pageable)
+                .map(each -> modelMapper.map(each, BookDTO.class));
+    }
+
+    public BookDTO update(Long id, Book object) {
+        Optional<Book> bookOptional = bookRepository.findById(id);
+
+        if(!bookOptional.isPresent()) {
+            return null;
+        }
+
+        Book book = bookOptional.get();
+        BeanUtils.copyProperties(object, book, "id");
+        save(book);
+
+        return modelMapper.map(book, BookDTO.class);
+    }
+
+    public void deleteById(Long id) {
+        bookRepository.deleteById(id);
+    }
 }
