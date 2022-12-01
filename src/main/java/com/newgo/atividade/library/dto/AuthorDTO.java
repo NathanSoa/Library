a62fe72d6d51373b06f1851689c757f93bcc04a0
NathanSoa <nathansoares809@gmail.com>
2022-12-01 11:22:38 -0300

    add: author dto object

---
+++ src/main/java/com/newgo/atividade/library/dto/AuthorDTO.java
@@ -0,0 +1,48 @@
+package com.newgo.atividade.library.dto;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+public class AuthorDTO {
+
+    private String firstName;
+    private String lastName;
+    private Set<String> books;
+    private LocalDate birthdate;
+
+    public AuthorDTO() {
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Set<String> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<String> books) {
+        this.books = books;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+}
